b4707d26d1bae31ee24bcf980b7828e99ab801d4
thomkeh <thomkeh@users.noreply.github.com>
2017-04-13 11:25:12 +0200

    Add the -j flag as the docs suggest

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd start-overlay-in-application-view@cis.net/
-zip ../start-overlay-in-application-view@cis.net extension.js metadata.json stylesheet.css
+zip -j ../start-overlay-in-application-view@cis.net extension.js metadata.json stylesheet.css
