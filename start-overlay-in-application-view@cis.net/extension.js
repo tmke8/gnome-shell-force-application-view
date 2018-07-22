27fe7416fafc3a98c5554361b51415d00e7ddf13
thomkeh <7741417+thomkeh@users.noreply.github.com>
2018-07-22 13:01:29 +0100

    Make some comments clearer

--- start-overlay-in-application-view@cis.net/extension.js
+++ start-overlay-in-application-view@cis.net/extension.js
@@ -13,7 +13,7 @@ let originalToggleFunction;
 
 // ......................................................................... //
 function init() {
-    // on intialization set the our original toggle holding variable to null
+    // on intialization set the original toggle holding variable to null
     originalToggleFunction = null;
 }
 
@@ -21,20 +21,20 @@ function init() {
 // ......................................................................... //
 function enable() {
 
-    // save the existing toggle function so we can use to to restore bits later
+    // save the existing toggle function so we can use it to restore bits later
     originalToggleFunction = Overview.Overview.prototype['toggle'];
 
     /*
     this is a copy of an existing toggle function but where we
-    show apps but hid the whole Overview.
-    TODO: currently we do not trigger animation so if the Overview is
-          toggled before the icons appear (can be toggled by other apps like
-          dash-to-dock) so if the hiding the Overview happens before animation
-          is complete or the next application shows only some apps (ones that)
-          were drawn before untoggling wil appear. So we either need to run
-          the animation or if we do not use it we do the same tricks 
-          dash-to-dock does where it hides the Overview, does speedy animation
-          and unhides. We need to look into "_animateVisible"
+    show apps and hide the whole Overview.
+    TODO: currently we do not trigger animation. This is a problem for the case
+          where the app icons have started to appear and before the appearing
+          animation is complete, the overview is toggled (can be toggled by other
+          apps like dash-to-dock). So then, the hiding of the Overview happens
+          before the appearing animation is complete with weird results. So we
+          either need to run the animation or if we do not use it, we do the same
+          tricks dash-to-dock does where it hides the Overview, does speedy
+          animation and unhides. We need to look into "_animateVisible".
     */
     Overview.Overview.prototype['toggle'] = 
         function startOverlayInApplicationViewToggle() {
@@ -51,16 +51,16 @@ function enable() {
         };
 
     /*
-    "re"-bind 'panel-main-menu' to our monkeypatched function needed because 
-    of race conditions. Specifically during the login or Gnome Shell
-    restart extension is enabled before the keybind is bound to the function by
-    a call from "_sessionUpdated" function in main.js all works well. 
+    "re"-bind 'panel-main-menu' to our monkeypatched function. Needed because 
+    of race conditions. Specifically, during the login or Gnome Shell
+    restart the extension is enabled before the keybind is bound to the function
+    by a call from "_sessionUpdated()" in main.js and all works well. 
 
     However, when Gnome Shell Screen Lock is activated it disables the 
-    extension as part of the normal process hence unpatching the toggle
+    extension as part of the normal process. Hence unpatching the toggle
     prototype back to the original function. Then when the Screen Lock is
-    deactivated it looks like _sessionUpdated is run before extension enabled
-    and monkeypatching happens after it hence not binding out toggle function.
+    deactivated, it looks like _sessionUpdated is run before extension enabling
+    and monkeypatching happens. In this case, our toggle function is not binded.
 
     So we manually bind it ourselves.
     */
@@ -78,12 +78,12 @@ function enable() {
 function disable() {
 
     /* 
-    if we have the original function, put it back to back and re-bind the
+    if we have the original function, put it back and re-bind the
     'panel-main-menu' to original toggle again. (See explanation in enable
     function)
     */
     if (originalToggleFunction !== null) {
-        // save the ogi
+        // put the original back
         Overview.Overview.prototype['toggle'] = originalToggleFunction;
         // "re"-bind 'panel-main-menu' to our monkeypatched function 
         Main.wm.setCustomKeybindingHandler(
